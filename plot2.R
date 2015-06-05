9987c7bbe3da0a91ae81f6d68b05ab62de1ff8b9
julienbalas <julien.balas@gmail.com>
2015-06-05 14:31:26 +0200

    forgot to save the file before commit/push, bam

--- plot2.R
+++ plot2.R
@@ -1,4 +1,6 @@
 data <- read.csv("household_power_consumption.txt", header=TRUE, sep=";", dec=".")
 
+deux <- subset(data, data$Date == "1/2/2007" |  data$Date == "2/2/2007")
 #data$DateTime <- strptime(paste(data$Date, data$Time), "%d/%m/%Y %H:%M:%s")
-#plot(
+
+with(deux, plot(Global_active_power ~ Time, main="sl"))
