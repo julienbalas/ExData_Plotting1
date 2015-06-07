ca67d4ab57d06aa8c3252347c93d8bddc656e19a
Julien Balas <julien.balas@gmail.com>
2015-06-07 12:34:18 +0200

    with numerical value, better

--- plot2.R
+++ plot2.R
@@ -1,5 +1,6 @@
 data <- read.csv("household_power_consumption.txt", header=TRUE, sep=";", dec=".")
 two <- subset(data, data$Date == "1/2/2007" |  data$Date == "2/2/2007")
+two$Global_active_power<-as.numeric(as.character(two$Global_active_power)) 
 two$Date2 <- as.Date(two$Date,format="%d/%m/%Y")
 dateTime<-as.POSIXct(paste(two$Date2,as.character(two$Time)))
 two<-data.frame(two,dateTime)
