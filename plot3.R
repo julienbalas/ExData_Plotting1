bd11d8efb0c279baa556bd31be38dc351ef7fe64
Julien Balas <julien.balas@gmail.com>
2015-06-07 12:41:13 +0200

    with better label in legend

--- plot3.R
+++ plot3.R
@@ -1,5 +1,4 @@
-par(mfrow = c(1,1))
-
+par(mfrow = c(1,1),cex=.85,cex.axis=.85,cex.lab=.85)
 data <- read.csv("household_power_consumption.txt", header=TRUE, sep=";", dec=".")
 two <- subset(data, data$Date == "1/2/2007" |  data$Date == "2/2/2007")
 two$Date2 <- as.Date(two$Date,format="%d/%m/%Y")
@@ -18,4 +17,4 @@ with(two, lines(dateTime, Sub_metering_3, col="blue"))
 
 legend("topright", 
        lwd = 1,  lty = c(1, 1, 1), col = c( "black", "blue", "red" ), 
-       legend = c("Sub_metering_1" ,"Sub_metering_2" ,"Sub_metering_3" ))
+       legend = c("Sub_metering_1     " ,"Sub_metering_2" ,"Sub_metering_3" ))
