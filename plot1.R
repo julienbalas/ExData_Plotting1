75bb44b29a9c4512ae223db49d74845337f68e90
Julien Balas <julien.balas@gmail.com>
2015-06-07 12:16:25 +0200

    with filtering in R

--- plot1.R
+++ plot1.R
@@ -1,12 +1,4 @@
-#cat household_power_consumption.txt | head -1 > 2days.csv
-#cat household_power_consumption.txt | grep "^1/2/2007" >> 2days.csv 
-#cat household_power_consumption.txt | grep "^2/2/2007" >> 2days.csv 
-donnees <- read.csv(file="/home/julien/coursera/ExploratoryDataAnalysis/2days.csv", header=TRUE, sep=";")
-
-#full <- read.csv(file="/home/julien/coursera/ExploratoryDataAnalysis/household_power_consumption.txt", header=TRUE, sep=";")
-#full$Date <- as.Date(full$Date, format = "%d/%m/%Y") 
-#donnees <- subset(full, Date == as.Date("2007-02-01") || Date == as.Date("2007-02-02"))
-
-#TODO filtrage
-
-hist(donnees$Global_active_power, col="red",main="Global Active Power", xlab="Global active power (kilowatts)")
+data <- read.csv("household_power_consumption.txt", header=TRUE, sep=";", dec=".")
+two <- subset(data, data$Date == "1/2/2007" |  data$Date == "2/2/2007")
+two$Global_active_power<-as.numeric(as.character(two$Global_active_power)) 
+hist(two$Global_active_power, col="red",main="Global Active Power", xlab="Global active power (kilowatts)")
